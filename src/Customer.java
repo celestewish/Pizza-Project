5d4303748857bf96bb6b3ef6d1915ffe3bffc79f
Mere Solace <btm7428@gmail.com>
2024-11-11 10:28:33 -0500

    Customer and Payment
    set up some basic methods for Customer
    
    created Payment class

---
+++ src/Customer.java
@@ -0,0 +1,83 @@
+import java.util.LinkedList;
+
+public class Customer {
+	private static int idCounter = 1000000;
+	private final int userID;
+	private String name;
+	private String email;
+	private String password;
+	private String address;
+	private LinkedList<String> phoneNumbers;
+	private LinkedList<Payment> savedPaymentMethods;
+	
+	Customer(String name, String email, String password, String address, String phoneNumber) {
+		userID = idCounter;
+		idCounter++;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.address = address;
+		phoneNumbers.addFirst(phoneNumber);
+	}
+	
+	public int getUserID() {
+		return userID;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		// if (newName.split(" ").length != 3)
+		this.name = name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public boolean checkPassword(String password) {
+		return this.password.equals(password);
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public LinkedList<String> getPhoneNumbers() {
+		return phoneNumbers;
+	}
+	
+	public String getPhoneNumberAtIndex(int index) {
+		return phoneNumbers.get(index);
+	}
+	
+	public void addPhoneNumber(String phoneNumber) {
+		if (phoneNumbers.contains(phoneNumber)) {
+			return;
+		}
+		phoneNumbers.addLast(phoneNumber);
+	}
+	
+	public boolean removePhoneNumber(String phoneNumber) {
+		if (phoneNumbers.contains(phoneNumber)) {
+			phoneNumbers.remove(phoneNumber);
+			return true;
+		}
+		else
+			return false;
+	}
+}
---
+++ src/Payment.java
@@ -0,0 +1,6 @@
+public class Payment {
+	private String nameOnCard;
+	private String cardNumber;
+	private String expirationDate;
+	private int securityCode;
+}
