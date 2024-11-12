b55bd52594a54077e5458178b18f192a2c5e92c9
Lauren Rousell <laurenrousell@outlook.com>
2024-11-12 11:02:20 -0500

    added menu

---
+++ src/Menu.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+
+public class Menu {
+    private LinkedList<MenuItem> order = new LinkedList<>();
+    public Menu() {
+        order = null;
+    }
+    public void add(MenuItem item) {
+        order.add(item);
+    }
+    public void remove(MenuItem item) {
+        order.remove(item);
+    }
+    public float returnTotalCost(){
+        float total = 0;
+        for(MenuItem item : order){
+            total += item.getCost();
+        }
+        return total;
+    }
+}
