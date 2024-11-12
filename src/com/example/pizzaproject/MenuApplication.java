31bc3d588c4c10b813d2a78f448b925c49bee877
Lauren Rousell <laurenrousell@outlook.com>
2024-11-12 11:24:08 -0500

    ui stuff added
    I added javafx to our project, created a menucontroller, and created a menuapplication.

---
+++ src/com/example/pizzaproject/MenuApplication.java
@@ -0,0 +1,11 @@
+package com.example.pizzaproject;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+//this will be used for programming the menu gui's appearance
+public class MenuApplication {
+}
---
+++ src/com/example/pizzaproject/MenuController.java
@@ -0,0 +1,8 @@
+package com.example.pizzaproject;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+//this will be used for programming the menu buttons
+public class MenuController {
+}
