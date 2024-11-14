d0d5b95ff5c4b262ef47e898416ed8b05af07e80
Lauren Rousell <laurenrousell@outlook.com>
2024-11-14 11:27:20 -0500

    added meats and vegetables to pizza creation
    last thing i need to do for menu creation is add the other menu items

--- src/MenuGUI.java
+++ src/MenuGUI.java
@@ -20,6 +20,7 @@ public class MenuGUI {
                 createPizza();
             }
         });
+
     }
     public static void createPizza() {
         JFrame frame = new JFrame();
@@ -60,5 +61,117 @@ public class MenuGUI {
         JLabel cheeseTitle = new JLabel("Cheese");
         frame.add(cheeseTitle);
         JButton regularCheese = new JButton("Regular");
+
+        //toppings
+        JLabel toppingsTitle = new JLabel("Meats");
+        //meats
+        JMenuBar meatBar = new JMenuBar();
+        frame.setJMenuBar(meatBar);
+        JMenu meatMenu = new JMenu("Meats");
+        meatBar.add(meatMenu);
+        JMenu pepperoni = new JMenu("Pepperoni");
+        meatMenu.add(pepperoni);
+        JMenu bacon = new JMenu("Bacon");
+        meatMenu.add(bacon);
+        JMenu chicken = new JMenu("Chicken");
+        meatMenu.add(chicken);
+        JMenu groundBeef = new JMenu("Ground Beef");
+        meatMenu.add(groundBeef);
+
+        //meat submenus
+        //pepperoni
+        JMenuItem leftPepperoni = new JMenuItem("Left");
+        pepperoni.add(leftPepperoni);
+        JMenuItem rightPepperoni = new JMenuItem("Right");
+        pepperoni.add(rightPepperoni);
+        JMenuItem fullPepperoni = new JMenuItem("Full");
+        pepperoni.add(fullPepperoni);
+        JCheckBoxMenuItem extraPepperoni = new JCheckBoxMenuItem("Extra");
+        pepperoni.add(extraPepperoni);
+
+        //bacon
+        JMenuItem leftBacon = new JMenuItem("Left");
+        bacon.add(leftBacon);
+        JMenuItem rightBacon = new JMenuItem("Right");
+        bacon.add(rightBacon);
+        JMenuItem fullBacon = new JMenuItem("Full");
+        bacon.add(fullBacon);
+        JCheckBoxMenuItem extraBacon = new JCheckBoxMenuItem("Extra");
+        bacon.add(extraBacon);
+
+        //chicken
+        JMenuItem leftChicken = new JMenuItem("Left");
+        chicken.add(leftChicken);
+        JMenuItem rightChicken = new JMenuItem("Right");
+        chicken.add(rightChicken);
+        JMenuItem fullChicken = new JMenuItem("Full");
+        chicken.add(fullChicken);
+        JCheckBoxMenuItem extraChicken = new JCheckBoxMenuItem("Extra");
+        chicken.add(extraChicken);
+
+        //ground beef
+        JMenuItem leftBeef = new JMenuItem("Left");
+        bacon.add(leftBeef);
+        JMenuItem rightBeef = new JMenuItem("Right");
+        bacon.add(rightBeef);
+        JMenuItem fullBeef = new JMenuItem("Full");
+        bacon.add(fullBeef);
+        JCheckBoxMenuItem extraBeef = new JCheckBoxMenuItem("Extra");
+        bacon.add(extraBeef);
+
+        //vegetables
+        JMenuBar vegetablesBar = new JMenuBar();
+        frame.setJMenuBar(vegetablesBar);
+        JMenu vegetablesMenu = new JMenu("Vegetables");
+        vegetablesBar.add(vegetablesMenu);
+        JMenu spinach = new JMenu("Spinach");
+        vegetablesMenu.add(spinach);
+        JMenu onions = new JMenu("Onions");
+        vegetablesMenu.add(onions);
+        JMenu olives = new JMenu("Olives");
+        vegetablesMenu.add(olives);
+        JMenu peppers = new JMenu("Peppers");
+        vegetablesMenu.add(peppers);
+
+        //vegetable submenus
+        //spinach
+        JMenuItem leftSpinach = new JMenuItem("Left");
+        spinach.add(leftSpinach);
+        JMenuItem rightSpinach = new JMenuItem("Right");
+        spinach.add(rightSpinach);
+        JMenuItem fullSpinach = new JMenuItem("Full");
+        spinach.add(fullSpinach);
+        JCheckBoxMenuItem extraSpinach = new JCheckBoxMenuItem("Extra");
+        spinach.add(extraSpinach);
+
+        //onions
+        JMenuItem leftOnions = new JMenuItem("Left");
+        onions.add(leftOnions);
+        JMenuItem rightOnions = new JMenuItem("Right");
+        onions.add(rightOnions);
+        JMenuItem fullOnions = new JMenuItem("Full");
+        onions.add(fullOnions);
+        JCheckBoxMenuItem extraOnions = new JCheckBoxMenuItem("Extra");
+        onions.add(extraOnions);
+
+        //olives
+        JMenuItem leftOlives = new JMenuItem("Left");
+        olives.add(leftOlives);
+        JMenuItem rightOlives = new JMenuItem("Right");
+        olives.add(rightOlives);
+        JMenuItem fullOlives = new JMenuItem("Full");
+        olives.add(fullOlives);
+        JCheckBoxMenuItem extraOlives = new JCheckBoxMenuItem("Extra");
+        olives.add(extraOlives);
+
+        //peppers
+        JMenuItem leftPeppers = new JMenuItem("Left");
+        peppers.add(leftPeppers);
+        JMenuItem rightPeppers = new JMenuItem("Right");
+        peppers.add(rightPeppers);
+        JMenuItem fullPeppers = new JMenuItem("Full");
+        peppers.add(fullPeppers);
+        JCheckBoxMenuItem extraPeppers = new JCheckBoxMenuItem("Extra");
+        peppers.add(extraPeppers);
     }
 }
