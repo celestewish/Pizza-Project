3930689038b716aa9cf90c540993ebfda524a155
UliSteveV <72050786+UliSteveV@users.noreply.github.com>
2024-11-15 16:13:49 -0500

    Small changes to checkout

--- src/CheckOut.java
+++ src/CheckOut.java
@@ -1,15 +1,11 @@
 import javax.swing.*;
 
 
-public class CheckOut {
+public class CheckOut{
     private JPanel panel1;
-    private JPanel TopPart;
-    private JButton homeButton;
-    private JButton dealsButton;
-    private JButton menuButton;
-    private JButton locationsButton;
-    private JTextField checkoutTextField;
-    private JTextField yourInfoTextField;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
 
 
 }
