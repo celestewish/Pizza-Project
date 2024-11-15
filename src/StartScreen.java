c0414bd4ebc98032f0f6f3d206bd34073d6b2830
ahenshaw2003 <clashtrobot@gmail.com>
2024-11-15 15:30:53 -0500

    start screen changes

--- src/StartScreen.java
+++ src/StartScreen.java
@@ -5,12 +5,12 @@ import java.awt.event.ActionListener;
 
 public class StartScreen extends CardScreen {
 	private JPanel pnlStartScreen;
-	
+
 	private JButton btnLogin;
 	private JButton btnSignIn;
 	private JButton btnMenu;
 	private JButton btnHome;
-	
+
 	public StartScreen(CardLayout screenLayoutController, JPanel screenContainer) {
 		super(screenLayoutController, screenContainer);
 		
