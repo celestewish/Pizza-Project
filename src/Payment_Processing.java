681e14bebed61e93ea6a9e6a60dc98c7e118e301
CoreasJamie <coreasjamie@gmail.com>
2024-11-16 18:45:03 -0500

    Payment Processing
    Processing

---
+++ src/Payment_Processing.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Payment_Processing extends CardScreen{
+    public Payment_Processing(CardLayout screenLayoutController, JPanel screenContainer, ProgramInfo info) {
+        super(screenLayoutController, screenContainer, info);
+    }
+}
