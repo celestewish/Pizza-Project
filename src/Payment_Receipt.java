525aee31a3efd6f41149e70ccb33fed9e6d04c34
CoreasJamie <coreasjamie@gmail.com>
2024-11-16 20:22:31 -0500

    Cart update

--- src/Payment_Receipt.java
+++ src/Payment_Receipt.java
@@ -5,8 +5,13 @@ public class Payment_Receipt extends CardScreen{
 
 
     private JPanel Home;
+    private JButton btnBack;
 
     public Payment_Receipt(CardLayout screenLayoutController, JPanel screenContainer, ProgramInfo info) {
         super(screenLayoutController, screenContainer, info);
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
     }
 }
