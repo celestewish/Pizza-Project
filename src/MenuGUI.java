46e900d8a6a7c975b91a06aa8493c99468c76d31
Lauren Rousell <laurenrousell@outlook.com>
2024-11-16 19:44:48 -0500

    menu updates

--- src/MenuGUI.java
+++ src/MenuGUI.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class MenuGUI extends CardScreen{
@@ -37,11 +39,39 @@ public class MenuGUI extends CardScreen{
     private JTextArea $PriceTextArea5;
     private JComboBox comboBox5;
     private JButton addButton3;
+    private JButton addButton4;
 
     public MenuGUI(CardLayout screenLayoutController, JPanel screenContainer, ProgramInfo info) {
         super(screenLayoutController, screenContainer, info);
         createButton.addActionListener(_ -> showScreen("PizzaGUI"));
         homeButton.addActionListener(_ -> showScreen("StartScreen"));
+        addButton.addActionListener(_ -> {
+            Drink drink = (Drink) Add.getSelectedItem();
+        });
+        addButton2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Dessert dessert = (Dessert) Add.getSelectedItem();
+            }
+        });
+        addButton4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Side wings = (Side) Add.getSelectedItem();
+            }
+        });
+        addButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Side garlicBread = (Side) Add.getSelectedItem();
+            }
+        });
+        addButton3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Side garlicKnots = (Side) Add.getSelectedItem();
+            }
+        });
     }
     public JPanel getScreenPanel(){
         return panel1;
