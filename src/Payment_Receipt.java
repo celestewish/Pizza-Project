43a82596e4a20ed2601ff2ddb7678958f8c36aa2
UliSteveV <72050786+UliSteveV@users.noreply.github.com>
2024-11-16 23:41:14 -0500

    Fixed Menu.Java

--- src/Payment_Receipt.java
+++ src/Payment_Receipt.java
@@ -8,9 +8,25 @@ public class Payment_Receipt extends CardScreen{
 
     private JPanel Home;
     private JButton btnHome;
+    private JLabel OrderPlace;
+    private JLabel ThankYou;
+    private JLabel OrderNumber;
+    private JLabel OrderSummary;
+    private JLabel ItemType;
+    private JLabel ListIngred;
+    private JLabel Total;
+    private JLabel PaymentMethod;
+    private JLabel TotalInfo;
+    private JLabel Carry;
+    private JLabel StorePhone;
+    private JLabel StoreAddress;
+    private JLabel EstTime;
 
     public Payment_Receipt(CardLayout screenLayoutController, JPanel screenContainer, ProgramInfo info) {
         super(screenLayoutController, screenContainer, info);
         btnHome.addActionListener(e -> showScreen("StartScreen"));
     }
+
+    public JPanel getScreenPanel(){return Home;}
+
 }
