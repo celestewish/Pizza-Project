70a41f7f9ab8171384bcc980c093fc17d2bb1033
CoreasJamie <coreasjamie@gmail.com>
2024-11-16 20:31:51 -0500

    update button

--- src/Payment_Receipt.java
+++ src/Payment_Receipt.java
@@ -1,12 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Payment_Receipt extends CardScreen{
 
 
     private JPanel Home;
+    private JButton btnHome;
 
     public Payment_Receipt(CardLayout screenLayoutController, JPanel screenContainer, ProgramInfo info) {
         super(screenLayoutController, screenContainer, info);
+        btnHome.addActionListener(e -> showScreen("StartScreen"));
     }
 }
