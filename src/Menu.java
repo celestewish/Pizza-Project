6354d10219512001ba7d585f701004a4dd332f2d
Mere Solace <btm7428@gmail.com>
2024-11-16 19:35:31 -0500

    Changed Menu

--- src/Menu.java
+++ src/Menu.java
@@ -1,25 +1,44 @@
-import java.util.LinkedList;
+import javax.swing.*;
+import java.awt.*;
 
-public class Menu {
-    private LinkedList<MenuItem> order = new LinkedList<>();
-    
-    public Menu() {
-        order = null;
-    }
-    
-    public void add(MenuItem item) {
-        order.add(item);
-    }
-    
-    public void remove(MenuItem item) {
-        order.remove(item);
-    }
-    
-    public float returnTotalCost(){
-        float total = 0;
-        for(MenuItem item : order){
-            total += item.getCost();
-        }
-        return total;
-    }
+public class Menu extends CardScreen{
+	
+	private JButton btnDeals;
+	private JButton btnLocations;
+	private JButton btnHome;
+	private JButton btnMenu;
+	private JButton btnSignIn;
+	private JButton placeYourOrderButton;
+	private JLabel lblTotalCost;
+	private JButton btnCreatePizza;
+	private JLabel lblPizzaPrice;
+	private JButton btnViewOrder;
+	private JLabel lblDrinkPrice;
+	private JLabel lblDessertPrice;
+	private JLabel lblWingPrice;
+	private JButton btnAddDrink;
+	private JButton btnAddDessert;
+	private JButton btnAddSalad;
+	private JButton btnAddGarlicBread;
+	private JButton btnAddWing;
+	private JLabel lblGarlicPrice;
+	private JLabel lblSaladPrice;
+	private JComboBox optDrinkType;
+	private JComboBox optDrinkSize;
+	private JComboBox optDrinkCt;
+	private JComboBox optDessertType;
+	private JComboBox optDessertCt;
+	private JComboBox optWingType;
+	private JComboBox optWingSize;
+	private JComboBox optWingCt;
+	private JComboBox optGarlicSize;
+	private JComboBox optGarlicCt;
+	private JComboBox optSaladType;
+	private JComboBox optSaladDressing;
+	private JComboBox optSaladCount;
+	
+	public Menu(CardLayout screenLayoutController, JPanel screenContainer, ProgramInfo info) {
+		super(screenLayoutController, screenContainer, info);
+
+	}
 }
