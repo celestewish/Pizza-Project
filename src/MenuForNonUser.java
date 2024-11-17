f64cceac77b05e360845039d3048b55be6ce9f4e
Mere Solace <btm7428@gmail.com>
2024-11-16 23:04:16 -0500

    Non User Menu

---
+++ src/MenuForNonUser.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MenuForNonUser extends CardScreen {
+	
+	
+	private JButton btnHome;
+	private JButton btnMenu;
+	private JButton btnSignIn_SignUp;
+	private JButton btnDeals;
+	private JButton btnLocations;
+	private JPanel pnlMenu;
+	private JButton btnCreateAccount;
+	private JTextArea cokeDietCokePepsiTextArea;
+	private JTextArea cookieBrowniesCinnamonKnotsTextArea;
+	
+	public MenuForNonUser(CardLayout screenLayoutController, JPanel screenContainer, ProgramInfo info) {
+		super(screenLayoutController, screenContainer, info);
+		
+		
+		btnHome.addActionListener(_ -> {
+			showScreen("StartScreen");
+		});
+		
+		btnDeals.addActionListener(_ -> {
+			showScreen("Deals");
+		});
+		
+		btnMenu.addActionListener(_ -> {
+			showScreen("Menu");
+		});
+		
+		btnSignIn_SignUp.addActionListener(_ -> {
+			showScreen("SignIn");
+		});
+		
+		btnLocations.addActionListener(_ -> {
+			showScreen("LocationScreen");
+		});
+		
+		btnCreateAccount.addActionListener(_ -> {
+			showScreen("SignUp");
+		});
+		
+		
+		
+	}
+}
