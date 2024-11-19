630aa3277e7c4999911f80bb70c6fcae4fa17bd9
CoreasJamie <coreasjamie@gmail.com>
2024-11-18 20:28:54 -0500

    Update favorite Orders

--- src/Favorite_Order.java
+++ src/Favorite_Order.java
@@ -1,2 +1,23 @@
+import javax.swing.*;
+
 public class Favorite_Order {
+    private JPanel pnlMenu;
+    private JButton btnHome;
+    private JButton btnMenu;
+    private JButton btnDeals;
+    private JButton btnLocations;
+    private JButton btnSignOut;
+    private JButton btnCart;
+    private JLabel lblHiName;
+    private JLabel lblCurTotal;
+    private JButton returnButton;
+    private JButton btnViewOrder;
+    private JButton btnOrders;
+    private JComboBox cboxCrust;
+    private JList list1;
+    private JCheckBox reorderThisCheckBox;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
