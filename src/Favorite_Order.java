dd70c86996057cebaaba9c3aaf331c4f64aa4b91
CoreasJamie <coreasjamie@gmail.com>
2024-11-18 21:00:57 -0500

    update again
    update to code

--- src/Favorite_Order.java
+++ src/Favorite_Order.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
+import java.awt.*;
 
-public class Favorite_Order {
+public class Favorite_Order extends CardScreen{
     private JPanel pnlMenu;
     private JButton btnHome;
     private JButton btnMenu;
@@ -16,7 +17,26 @@ public class Favorite_Order {
     private JComboBox cboxCrust;
     private JCheckBox selectCheckBox;
 
+    public Favorite_Order(CardLayout screenLayoutController, JPanel screenContainer, ProgramInfo info, String panelName) {
+        super(screenLayoutController, screenContainer, info, panelName);
+    }
+
     private void createUIComponents() {
         // TODO: place custom component creation code here
+    }
+
+    @Override
+    public boolean onAttemptLeaveScreen(ProgramInfo info) {
+        return false;
+    }
+
+    @Override
+    public Screen onAttemptEnterScreen(ProgramInfo info, Screen toScreen) {
+        return null;
+    }
+
+    @Override
+    public void onEnterScreen(ProgramInfo info) {
+
     }
 }
