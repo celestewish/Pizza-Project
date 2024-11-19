6010f105d2cc80e517009572f15fc514ecd4175f
Mere Solace <btm7428@gmail.com>
2024-11-18 22:00:40 -0500

    Update cart methods

--- src/Cart.java
+++ src/Cart.java
@@ -22,9 +22,9 @@ public class Cart extends CardScreen {
     private JButton placeYourOrderButton;
     private JLabel lblTotalCost;
     private JButton btnViewOrder;
-
-    public Cart (CardLayout screenLayoutController, JPanel screenContainer, ProgramInfo info, String panelName) {
-		super(screenLayoutController, screenContainer, info, panelName);
+	
+	public Cart (CardLayout screenLayoutController, JPanel screenContainer, String panelName) {
+		super(screenLayoutController, screenContainer, panelName);
 		setScreenPanel(pnlCart);
 		info.registerScreenName(Screen.CART, this);
 		screenContainer.add(this.getScreenPanel(), this.getPanelName());
@@ -33,17 +33,17 @@ public class Cart extends CardScreen {
 	}
 	
 	@Override
-	public boolean onAttemptLeaveScreen(ProgramInfo info) {
+	public boolean onAttemptLeaveScreen() {
 		return true;
 	}
 	
 	@Override
-	public Screen onAttemptEnterScreen(ProgramInfo info, Screen toScreen) {
+	public Screen onAttemptEnterScreen(Screen toScreen) {
 		return toScreen;
 	}
 	
 	@Override
-	public void onEnterScreen(ProgramInfo info) {
+	public void onEnterScreen() {
 	
 	}
 	
