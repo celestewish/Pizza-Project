b73f489e85357f94a5eacba3376ad8bff75975d6
ahenshaw2003 <clashtrobot@gmail.com>
2024-11-19 19:56:19 -0500

    pizzagui updates

--- src/PizzaGUI.java
+++ src/PizzaGUI.java
@@ -14,7 +14,7 @@ public class PizzaGUI extends CardScreen {
     private JButton btnLocations;
     private JButton btnSignOut;
     private JButton btnCart;
-    
+
     private JComboBox<?> cboxCrust;
     private JComboBox<?> cboxSize;
     private JComboBox<?> cboxSauce;
@@ -24,24 +24,30 @@ public class PizzaGUI extends CardScreen {
     private JLabel lblCurTotal;
 
     private boolean isContinuing = false;
-    
+
     public PizzaGUI(CardLayout screenLayoutController, JPanel screenContainer, String panelName) {
         super(screenLayoutController, screenContainer, panelName);
         setScreenPanel(pnlCreatePizza);
         info.registerScreenName(Screen.CREATE_PIZZA, this);
         screenContainer.add(this.getScreenPanel(), this.getPanelName());
-        
+
         final PizzaSize[] mySize = new PizzaSize[1];
         final CrustType[] myCrust = new CrustType[1];
-        final boolean[] sauce = new boolean[1];
+        final SauceOption[] mysauce = new SauceOption[1];
 
         setUpNavBar_LoggedIn(btnHome, btnMenu, btnDeals, btnLocations, btnSignOut, btnCart);
 
         btnContinue.addActionListener(_ -> {
-            isContinuing = true;
-            Pizza newPizza = new Pizza(mySize[0], myCrust[0], sauce[0]);
-            info.setCurPizza(newPizza);
-            showScreen(Screen.TOPPINGS);
+            // Check if any option is null
+            if ((mySize[0] == null) || (myCrust[0] == null) || (mysauce[0] == null)) {
+                JOptionPane.showMessageDialog(null, "Please select a size, crust, and sauce.");
+            } else {
+                isContinuing = true;
+                Pizza newPizza = new Pizza(mySize[0], myCrust[0], mysauce[0]);
+                info.setCurPizza(newPizza);
+                System.out.println(info.getCurPizza().toString());
+                showScreen(Screen.TOPPINGS);
+            }
         });
 
         cboxCrust.addActionListener(_ -> {
@@ -55,11 +61,11 @@ public class PizzaGUI extends CardScreen {
                 myCrust[0] = CrustType.THICK_CRUST;
             }
         });
-        
+
         cboxSize.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (cboxSize.getSelectedItem().toString().equalsIgnoreCase("small")){
+                if (Objects.requireNonNull(cboxSize.getSelectedItem()).toString().equalsIgnoreCase("small")){
                     mySize[0] = PizzaSize.SMALL;
                 }
                 else if (cboxSize.getSelectedItem().toString().equalsIgnoreCase("medium")){
@@ -77,37 +83,36 @@ public class PizzaGUI extends CardScreen {
         cboxSauce.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (cboxSauce.getSelectedItem().toString().equalsIgnoreCase("marinera")){
-                    sauce[0] = true;
+                if (Objects.requireNonNull(cboxSauce.getSelectedItem()).toString().equalsIgnoreCase("marinera")){
+                    mysauce[0] = SauceOption.MARINARA;
                 }
                 else if (cboxSauce.getSelectedItem().toString().equalsIgnoreCase("alfredo")){
-                    sauce[0] = false;
+                    mysauce[0] = SauceOption.ALFREDO;
                 }
             }
         });
     }
 
 
-    
+
     @Override
     public boolean onAttemptLeaveScreen() {
-        if(isContinuing){
-            isContinuing = false;
-            return true;
+        if(!isContinuing){
+            return showConfirmationDialog("Abandon Pizza?", "Yes, I want to abandon my pizza", "No, keep me here", "Are you sure?");
         }
-        return showConfirmationDialog("Abandon Pizza?", "Yes, I want to abandon my pizza", "No, keep me here", "Are you sure?");
+        return isContinuing;
     }
-    
+
     @Override
     public Screen onAttemptEnterScreen(Screen toScreen) {
         return toScreen;
     }
-    
+
     @Override
     public void onEnterScreen() {
-    
+
     }
-    
+
     private void createUIComponents() {
         pnlCartLogo = new ImagePanel("cart.png");
         pnlLogo = new ImagePanel("PizzaLogo.png");
