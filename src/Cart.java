8894182fdd93f3b5892c9615cfe1b857695cde60
CoreasJamie <coreasjamie@gmail.com>
2024-11-20 14:58:14 -0500

    idk

--- src/Cart.java
+++ src/Cart.java
@@ -25,6 +25,7 @@ public class Cart extends CardScreen {
 	private JLabel costNumber;
 	private JButton returnButton;
 	private JButton btnCheckout;
+	private JTextArea textArea1;
 
 	public Cart (CardLayout screenLayoutController, JPanel screenContainer, String panelName) {
 		super(screenLayoutController, screenContainer, panelName);
@@ -67,11 +68,15 @@ public class Cart extends CardScreen {
 			info.setCurPizza(null);
 		});
 		for (int i = 0; i < info.getCurOrder().getItems().size(); i++) {
+			textArea1.append(info.getCurOrder().getItems().get(i).toString() + "\n");
+			textArea1.setFont(info.getOptionsFont());
 			JLabel lblItem = new JLabel(info.getCurOrder().getItems().get(i).toString());
-			lblItem.setFont(optionsFont);//this is 
-			lblItem.setHorizontalAlignment(SwingConstants.CENTER);
+			lblItem.setFont(optionsFont);//this is
+			/*lblItem.setHorizontalAlignment(SwingConstants.CENTER);
 			lblItem.setVerticalAlignment(SwingConstants.CENTER);
 			lblItem.setText(info.getCurOrder().getItems().get(i).toString());
+
+			 */
 			JButton newEditButton = new JButton("Edit");
 			JButton newRemoveButton = new JButton("Remove");
 			orderPanel.add(lblItem);
